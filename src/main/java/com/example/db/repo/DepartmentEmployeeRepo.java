0b99165e5eab0686cdab434ffca706757cca8b32
justbegginer <blackcloudsinthesky@protonmail.com>
2023-11-17 17:36:34 +0300

    add some methods in repo

--- src/main/java/com/example/db/repo/DepartmentEmployeeRepo.java
+++ src/main/java/com/example/db/repo/DepartmentEmployeeRepo.java
@@ -4,10 +4,13 @@ import com.example.db.models.DepartmentEmployee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DepartmentEmployeeRepo extends JpaRepository<DepartmentEmployee, Integer> {
 
     Optional<DepartmentEmployee> findDepartmentEmployeeByEmployeeId(int id);
+
+    List<DepartmentEmployee> findAllByDepartmentId(int id);
 }
--- src/main/java/com/example/db/repo/ProjectRepo.java
+++ src/main/java/com/example/db/repo/ProjectRepo.java
@@ -4,6 +4,10 @@ import com.example.db.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepo extends JpaRepository<Project, Integer> {
+
+    List<Project> findAllByDepartmentId(int id);
 }
